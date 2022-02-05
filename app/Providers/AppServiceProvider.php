3073377f71cf28ca28fa2811627f5935d9f12f0b
vagrant <vagrant@homestead>
2022-02-05 08:29:36 +0000

    话题列表页面

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
 
-        //
+        // 5.5章 设置话题列表分页
+        \Illuminate\Pagination\Paginator::useBootstrap();
     }
 }
