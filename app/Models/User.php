289aa48cd5eb5a56b8f07500f60724c429079dd2
vagrant <vagrant@homestead>
2022-02-05 10:46:11 +0000

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // 5.9章  新增模型关联
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
