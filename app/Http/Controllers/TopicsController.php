e6ddab68d7ec4d83303b29cfe963085aa01f5b43
vagrant <vagrant@homestead>
2022-02-05 10:04:57 +0000

    话题排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,12 +14,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
         // 5.6章：方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存。
         // 后面即使是在遍历数据时使用到这两个关联属性，
         // 数据已经被预加载并缓存，因此不会再产生多余的 SQL 查询
-        $topics = Topic::with('user', 'category')->paginate(30);
+
+        // 5.8章 控制器中调用排序
+        $topics = $topic->withOrder($request->order)->with('user', 'category')->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
