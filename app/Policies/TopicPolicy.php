e6957b8fcc30196c20ea1a8359edc85e7410e167
vagrant <vagrant@homestead>
2022-02-06 08:56:39 +0000

    用户可以编辑话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -9,8 +9,9 @@ class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
-        return true;
+        // 6.6章 编辑帖子授权
+        // 在授权策略的类方法里，返回 true 即允许访问，反之返回 false 为拒绝访问。
+        return $topic->user_id == $user->id;
     }
 
     public function destroy(User $user, Topic $topic)
